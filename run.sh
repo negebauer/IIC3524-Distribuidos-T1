12a8d3698ece40412d5a51df6dfa42c385c4277b
negebauer <negebauer@uc.cl>
2017-05-10 15:13:20 -0300

    Default values for run script

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ rm parallel
 make
 
 # Image and param to use
-if [[ -z $1 ]]; then img_in='test/img_in/lena.png'; else img_in=$1; fi
-if [[ -z $2 ]]; then kernel='test/kernel/blur.txt'; else kernel=$2; fi
-if [[ -z $3 ]]; then img_out='test/img_out/lena_blur.txt'; else img_out=$3; fi
+img_def='lena'
+kernel_def='blur'
+if [[ -z $1 ]]; then img_in="test/img_in/${img_def}.png"; else img_in=$1; fi
+if [[ -z $2 ]]; then kernel="test/kernel/${kernel_def}.txt"; else kernel=$2; fi
+if [[ -z $3 ]]; then img_out="test/img_out/${img_def}_${kernel_def}.png"; else img_out=$3; fi
 
 # Run stuff
 print() {
