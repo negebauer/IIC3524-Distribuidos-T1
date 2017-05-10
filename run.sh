c2015d2912359bfc8fc24cb5a709aac9f0a63d26
negebauer <negebauer@uc.cl>
2017-05-10 09:48:08 -0300

    Run script with params

--- run.sh
+++ run.sh
@@ -5,16 +5,21 @@ rm parallel
 # Compile
 make
 
+# Image and param to use
+if [[ -z $1 ]]; then img_in='test/img_in/lena.png'; else img_in=$1; fi
+if [[ -z $2 ]]; then kernel='test/kernel/blur.txt'; else kernel=$2; fi
+if [[ -z $3 ]]; then img_out='test/img_out/lena_blur.txt'; else img_out=$3; fi
+
 # Run stuff
 print() {
   echo "--- $1 ---"
 }
 print 'Running secuential'
-time ./secuential
+time ./secuential $img_in $kernel $img_out
 print 'Done secuential'
 echo ''
 print 'Running parallel'
-time ./parallel
+time ./parallel $img_in $kernel $img_out
 print 'Done parallel'
 echo ''
 
