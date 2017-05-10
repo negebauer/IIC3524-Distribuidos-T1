6f06576a44196b56c49c1bf14c962de6b684cad1
negebauer <negebauer@uc.cl>
2017-05-10 15:20:50 -0300

    Copy T0 from edd

--- t1/t1.c
+++ t1/t1.c
@@ -1,7 +1,9 @@
+#include "../imagelib/imagelib.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-  /* code */
+int main(int argc, char *argv[]) {
+  /* El programa recibe 3 parametros */
   if (argc != 4) {
     printf("Modo de uso: %s <input.png> <kernel.txt> <output.png>\n", argv[0]);
     printf("\t<input.png> es la imagen a filtrar\n");
@@ -9,5 +11,92 @@ int main(int argc, char const *argv[]) {
     printf("\t<output.png> es donde se guardará la imagen resultante\n");
     return 1;
   }
+
+  /* Leemos la imagen a partir del archivo */
+  char *input_file = argv[1];
+  Image *img_in = img_png_read_from_file(input_file);
+  Image *img_out = img_png_read_from_file(input_file);
+
+  /* Let's read the kernel file */
+  const char *kernel_file_path = argv[2];
+  // printf("Reading %s\n", kernel_file_path);
+  FILE *kernel_file = fopen(kernel_file_path, "r");
+  int rows, cols;
+  fscanf(kernel_file, "%i %i", &rows, &cols);
+  int center_row = rows / 2;
+  int center_col = cols / 2;
+  float **kernel = malloc(rows * sizeof(float *));
+  for (int row = 0; row < rows; row++) {
+    kernel[row] = malloc(cols * sizeof(float));
+  }
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      if (!fscanf(kernel_file, "%f", &kernel[row][col])) {
+        break;
+      }
+    }
+  }
+  fclose(kernel_file);
+
+  // Log the kernel matrix
+  // printf("rows: %i cols: %i\n", rows, cols);
+  // printf("center [%i][%i]\n", center_row, center_col);
+  // for (int row = 0; row < rows; row++) {
+  //   printf("\n");
+  //   for (int col = 0; col < cols; col++) {
+  //     printf("%lf ", kernel[row][col]);
+  //   }
+  // }
+  // printf("\n");
+
+  // Let's create our new image
+  for (int img_row = 0; img_row < img_in->height; img_row++) {
+    for (int img_col = 0; img_col < img_in->width; img_col++) {
+      // Let's clear the image first
+      img_out->pixels[img_row][img_col].R = 0;
+      img_out->pixels[img_row][img_col].G = 0;
+      img_out->pixels[img_row][img_col].B = 0;
+
+      // Let's iterate over the kernel
+      for (int kernel_row = 0; kernel_row < rows; kernel_row++) {
+        for (int kernel_col = 0; kernel_col < cols; kernel_col++) {
+          // Let's apply the weighted pixels
+          int pixel_row = img_row - center_row + kernel_row;
+          int pixel_col = img_col - center_col + kernel_col;
+          float weight = kernel[kernel_row][kernel_col];
+          if (pixel_row < 0) {
+            pixel_row = 0;
+          } else if (pixel_row >= img_in->height) {
+            pixel_row = img_in->height - 1;
+          }
+          if (pixel_col < 0) {
+            pixel_col = 0;
+          } else if (pixel_col >= img_in->width) {
+            pixel_col = img_in->width - 1;
+          }
+          img_out->pixels[img_row][img_col].R +=
+              weight * img_in->pixels[pixel_row][pixel_col].R;
+          img_out->pixels[img_row][img_col].G +=
+              weight * img_in->pixels[pixel_row][pixel_col].G;
+          img_out->pixels[img_row][img_col].B +=
+              weight * img_in->pixels[pixel_row][pixel_col].B;
+        }
+      }
+    }
+  }
+
+  /* Guardamos la imagen como un archivo png */
+  char *output_file = argv[3];
+  img_png_write_to_file(img_out, output_file);
+
+  /* Liberamos los recursos asociados a la imagen */
+  img_destroy(img_in);
+  img_destroy(img_out);
+
+  // Liberamos la matriz del kernel
+  for (int row = 0; row < rows; row++) {
+    free(kernel[row]);
+  }
+  free(kernel);
   return 0;
 }
