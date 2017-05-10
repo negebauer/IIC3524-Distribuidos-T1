0a0eb80aeb24c286a7b2edcb140d56e4f1ecbbfe
negebauer <negebauer@uc.cl>
2017-05-10 09:48:13 -0300

    Check args

--- t1/t1.c
+++ t1/t1.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char const *argv[]) {
   /* code */
-  printf("Here we have to do everythang\n");
+  if (argc != 4) {
+    printf("Modo de uso: %s <input.png> <kernel.txt> <output.png>\n", argv[0]);
+    printf("\t<input.png> es la imagen a filtrar\n");
+    printf("\t<kernel.txt> es el filtro a usar\n");
+    printf("\t<output.png> es donde se guardará la imagen resultante\n");
+    return 1;
+  }
   return 0;
 }
