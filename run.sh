5c48ae57946b6afeac29cd55676a8856e50ff46b
negebauer <negebauer@uc.cl>
2017-05-11 20:15:32 -0300

    Rename some stuff, some some other stuff

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ make
 
 # Image and param to use
 img_def='big'
-kernel_def='blur'
+kernel_def='blur_10'
 if [[ -z $1 ]]; then img_in="test/img_in/${img_def}.png"; else img_in=$1; fi
 if [[ -z $2 ]]; then kernel="test/kernel/${kernel_def}.txt"; else kernel=$2; fi
 if [[ -z $3 ]]; then img_out="test/img_out/${img_def}_${kernel_def}.png"; else img_out=$3; fi
