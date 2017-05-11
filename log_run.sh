c90cd66c6304bbbd87558b978e886919a39a8829
negebauer <negebauer@uc.cl>
2017-05-11 20:49:49 -0300

    Running and logging

--- log_run.sh
+++ log_run.sh
@@ -1,5 +1,1 @@
-echo '' >> t1.log
-echo '-- Log Run --' >> t1.log
-(./run.sh) &>> t1.log
-echo '-- End Run --' >> t1.log
-echo '' >> t1.log
+(./run.sh) >> t1.log
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+echo "### START ###"
+
 # Clear previous compiled
 rm secuential
 rm parallel
@@ -6,23 +8,28 @@ rm parallel
 make
 
 # Image and param to use
-img_def='really_big'
+img_def='big'
 kernel_def='blur_10'
 if [[ -z $1 ]]; then img_in="test/img_in/${img_def}.png"; else img_in=$1; fi
 if [[ -z $2 ]]; then kernel="test/kernel/${kernel_def}.txt"; else kernel=$2; fi
 if [[ -z $3 ]]; then img_out="test/img_out/${img_def}_${kernel_def}.png"; else img_out=$3; fi
+echo "
+Apply mask
+  $kernel
+to image
+  $img_in
+"
 
 # Run stuff
 print() {
   echo "--- $1 ---"
 }
-print 'Running secuential'
+print 'Start secuential'
 time ./secuential $img_in $kernel $img_out
 print 'Done secuential'
 echo ''
-print 'Running parallel'
+print 'Start parallel'
 time ./parallel $img_in $kernel $img_out
 print 'Done parallel'
-echo ''
 
-echo 'Done!'
+echo "### DONE ###"
