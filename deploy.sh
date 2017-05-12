0a73f53d6913ce1fbdb57fc40afce7420dac3654
negebauer <negebauer@uc.cl>
2017-05-11 21:00:34 -0300

    Update deploy command to use rsync

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+# cd -- "$(dirname "$BASH_SOURCE")"
+# cd ..
+# source ~/Bash/uc.sh
+# tripioupload T1
+
 cd -- "$(dirname "$BASH_SOURCE")"
-cd ..
-source ~/Bash/uc.sh
-tripioupload T1
+rsync -azP ../T1/ negebauer@hercules.ing.puc.cl:T1/
