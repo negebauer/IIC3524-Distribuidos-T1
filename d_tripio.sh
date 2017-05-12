679b35093091d46d35bd3cb8efc0384c7af1e25e
negebauer <negebauer@uc.cl>
2017-05-11 21:43:51 -0300

    Update exclude and tripio deploy

--- d_tripio.sh
+++ d_tripio.sh
@@ -1,6 +1,7 @@
+folder='t1_tripio'
 cd -- "$(dirname "$BASH_SOURCE")"
 source ~/Bash/uc.sh
-hercules "rsync -e 'ssh -i ~/.ssh/tripio' -azP ~/t1_hercules/ tripio:t1"
+hercules "rsync -e 'ssh -i ~/.ssh/tripio' -azP ~/t1_hercules/ tripio:${folder}"
 
 source ~/Bash/uc.sh
-tripio "chmod +x run.sh log_run.sh"
+tripio "chmod +x ${folder}/run.sh ${folder}/log_run.sh"
