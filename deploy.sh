913af2d33ce963c7fbd2129a67e9897c044ea9e4
negebauer <negebauer@uc.cl>
2017-05-11 21:21:11 -0300

    Delete comments from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,2 @@
-# cd -- "$(dirname "$BASH_SOURCE")"
-# cd ..
-# source ~/Bash/uc.sh
-# tripioupload T1
-
 cd -- "$(dirname "$BASH_SOURCE")"
 rsync -azP --exclude-from 'rsync_exclude.txt' ../t1/ negebauer@hercules.ing.puc.cl:t1
